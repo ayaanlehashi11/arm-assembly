e2bb79e79ad5afe8de4a84950cff8f6e718d8a27
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2023-05-28 13:54:35 +0300

    Add files via upload

---
+++ arithmetic/add.s
@@ -0,0 +1,26 @@
+
+x:
+        .word   5
+y:
+        .word   8
+.LC0:
+        .string "The sum is %d\n"
+main:
+        stp     x29, x30, [sp, -32]!
+        mov     x29, sp
+        adrp    x0, x
+        add     x0, x0, :lo12:x
+        ldr     w1, [x0]
+        adrp    x0, y
+        add     x0, x0, :lo12:y
+        ldr     w0, [x0]
+        add     w0, w1, w0
+        str     w0, [sp, 28]
+        ldr     w1, [sp, 28]
+        adrp    x0, .LC0
+        add     x0, x0, :lo12:.LC0
+        bl      printf
+        mov     w0, 0
+        ldp     x29, x30, [sp], 32
+        ret
+    
---
+++ arithmetic/sub.s
@@ -0,0 +1,27 @@
+x:
+        .word   14
+y:
+        .word   7
+.LC0:
+        .ascii  "The difference is %d\012\000"
+main:
+        push    {r7, lr}
+        sub     sp, sp, #8
+        add     r7, sp, #0
+        movw    r3, #:lower16:x
+        movt    r3, #:upper16:x
+        ldr     r2, [r3]
+        movw    r3, #:lower16:y
+        movt    r3, #:upper16:y
+        ldr     r3, [r3]
+        subs    r3, r2, r3
+        str     r3, [r7, #4]
+        ldr     r1, [r7, #4]
+        movw    r0, #:lower16:.LC0
+        movt    r0, #:upper16:.LC0
+        bl      printf
+        movs    r3, #0
+        mov     r0, r3
+        adds    r7, r7, #8
+        mov     sp, r7
+        pop     {r7, pc}
